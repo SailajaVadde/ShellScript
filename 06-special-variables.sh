65fba91799bac3a4b10eeaeb742e0d5057b3fa39
SailajaVadde <165315537+SailajaVadde@users.noreply.github.com>
2024-04-16 17:59:11 -0500

    Update 06-special-variables.sh

--- 06-special-variables.sh
+++ 06-special-variables.sh
@@ -8,4 +8,4 @@ echo "Home directory of current user: $HOME"
 echo "which user is running this script: $USER"
 echo "Hostname: $HOSTNAME"
 echo "process id of current shell script: $$"
-
+echo "To know the previous command: $?"

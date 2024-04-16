4b71af10d350f692760657203085eb01dad89be4
SailajaVadde <165315537+SailajaVadde@users.noreply.github.com>
2024-04-16 17:52:03 -0500

    Update 06-special-variables.sh

--- 06-special-variables.sh
+++ 06-special-variables.sh
@@ -7,3 +7,5 @@ echo "Current working directory $PWD"
 echo "Home directory of current user: $HOME"
 echo "which user is running this script: $USER"
 echo "Hostname: $HOSTNAME"
+echo "process id of current shell script: $$"
+

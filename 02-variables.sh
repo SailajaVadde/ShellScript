3a7bedd10dadb69423e9a7a4973e1cd7a4e0efed
SailajaVadde <165315537+SailajaVadde@users.noreply.github.com>
2024-04-16 15:46:23 -0500

    Update 02-variables.sh

--- 02-variables.sh
+++ 02-variables.sh
@@ -1,7 +1,10 @@
 #!/bin/bash
 
+PERSON1=Ramesh
+PERSON2=Suresh
 
-echo "Ramesh:: Hello suresh how r u ?"
-echo "Suresh:: I am doing good. What about you?"
-echo "I am also good. what is your next paln"
-echo "Not yet decided"
+
+echo "$PERSON1:: Hello $PERSON2 how r u ?"
+echo "$PERSON2:: Hello $PERSON1 I am doing good. What about you?"
+echo "$PERSON1:: I am also good. what is your next paln"
+echo "$PERSON2:: Not yet decided"

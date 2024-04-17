8f70303cb08cd3e60e7dfe5b474c49678b8bbc7b
SailajaVadde <165315537+SailajaVadde@users.noreply.github.com>
2024-04-17 12:25:08 -0500

    Update 07-installation.sh

--- 07-installation.sh
+++ 07-installation.sh
@@ -4,7 +4,7 @@ USERID=$(id -u)
 
 if [ $USERID -ne 0 ]
 then
-    echo"please run this script with root access"
+    echo "please run this script with root access"
     exit 1 # manually exit if error comes
 
 else 

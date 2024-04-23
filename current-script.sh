afa0e48f6d0f678f0dc233d2cf89eb7e73a2e87a
Sailaja Vadde <Sailu@Shrihan.attlocal.net>
2024-04-23 15:17:27 -0500

    New script

---
+++ current-script.sh
@@ -0,0 +1,11 @@
+#1/bin/bash
+
+COURSE="Devops from current script"
+
+echo "Before calling other script, course: $COURSE"
+echo "Process id of current shell script: $$"
+
+#./other-script.sh
+
+echo "After calling other script, course: $COURSE"
+

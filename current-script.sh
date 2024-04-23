856b71caec978f5824d922cab7e2061329967116
Sailaja Vadde <Sailu@Shrihan.attlocal.net>
2024-04-23 15:46:34 -0500

    Calling 2nd way

--- current-script.sh
+++ current-script.sh
@@ -5,7 +5,9 @@ COURSE="Devops from current script"
 echo "Before calling other script, course: $COURSE"
 echo "Process id of current shell script: $$"
 
-./other-script.sh
+#./other-script.sh
+
+source ./other-script.sh
 
 echo "After calling other script, course: $COURSE"
 

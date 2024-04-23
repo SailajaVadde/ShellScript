bd090eb6331cd9bedcb49d9cfadee02de9c690a9
Sailaja Vadde <Sailu@Shrihan.attlocal.net>
2024-04-23 15:31:24 -0500

    New file added

--- current-script.sh
+++ current-script.sh
@@ -5,7 +5,7 @@ COURSE="Devops from current script"
 echo "Before calling other script, course: $COURSE"
 echo "Process id of current shell script: $$"
 
-#./other-script.sh
+./other-script.sh
 
 echo "After calling other script, course: $COURSE"
 
